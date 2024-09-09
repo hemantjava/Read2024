53e6521c6f2c819a85e39c3e60c384590160de1c
Hemant Kumar sahu <Hemantjava90@gmail.com>
2024-09-09 16:01:38 +0530

    Update Odd and Even printing logic

--- DSA/src/main/java/org/dsa/interview/mulithreading/completableFuture/OddAndEvenMultiThreading.java
+++ DSA/src/main/java/org/dsa/interview/mulithreading/completableFuture/OddAndEvenMultiThreading.java
@@ -3,41 +3,34 @@ package org.dsa.interview.mulithreading.completableFuture;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.Logger;
 import java.util.stream.IntStream;
-
 public class OddAndEvenMultiThreading {
     public static void main(String[] args) {
-        final int maxNum = 20;
-        printOddAndEvenNumber(maxNum);
+        printOddAndEvenNumber(20);
     }
 
     private static void printOddAndEvenNumber(int maxNum) {
-        CompletableFuture oddFuture = CompletableFuture.runAsync(()->{
-            IntStream.rangeClosed(1, maxNum)
-                    .filter(num -> num%2 != 0)
-                    .forEach(num -> {
-                        System.out.println(num+": Odd");
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
+        CompletableFuture<Void> oddFuture = CompletableFuture.runAsync(()-> IntStream.rangeClosed(1, maxNum)
+                .filter(num -> num%2 != 0)
+                .forEach(num -> {
+                    System.out.println(num+": Odd");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
 
-                    });
-        });
-        CompletableFuture evenFuture = CompletableFuture.runAsync(()->{
-            IntStream.rangeClosed(1, maxNum)
-                    .filter(num -> num%2 == 0)
-                     .forEach(num -> {
-                         System.out.println(num+": Even");
-                         try {
-                             Thread.sleep(1000);
-                         } catch (InterruptedException e) {
-                             throw new RuntimeException(e);
-                         }
-                     });
-        });
+                }));
+        CompletableFuture<Void> evenFuture = CompletableFuture.runAsync(()-> IntStream.rangeClosed(1, maxNum)
+                .filter(num -> num%2 == 0)
+                 .forEach(num -> {
+                     System.out.println(num+": Even");
+                     try {
+                         Thread.sleep(1000);
+                     } catch (InterruptedException e) {
+                         throw new RuntimeException(e);
+                     }
+                 }));
         CompletableFuture<Void> allOf = CompletableFuture.allOf(oddFuture,evenFuture);
         allOf.join();
     }
